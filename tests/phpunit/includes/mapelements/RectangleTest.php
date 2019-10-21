75f21fa154ee1b95b114ea199a37d5c90f6b1ae5
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2019-10-21 15:12:59 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 28.0.0
    The following sniffs are failing and were disabled:
    * PSR12.Properties.ConstantVisibility.NotFound
    
    Change-Id: I5698607b79848547579488940025313033374d1d

--- tests/phpunit/includes/mapelements/RectangleTest.php
+++ tests/phpunit/includes/mapelements/RectangleTest.php
@@ -13,7 +13,7 @@ class RectangleTest extends \PHPUnit\Framework\TestCase {
 	 * Sets up the fixture, for example, opens a network connection.
 	 * This method is called before a test is executed.
 	 */
-	protected function setUp() {
+	protected function setUp() : void {
 		$this->object = new Rectangle;
 	}
 
@@ -21,7 +21,7 @@ class RectangleTest extends \PHPUnit\Framework\TestCase {
 	 * Tears down the fixture, for example, closes a network connection.
 	 * This method is called after a test is executed.
 	 */
-	protected function tearDown() {
+	protected function tearDown() : void {
 	}
 
 	/**
